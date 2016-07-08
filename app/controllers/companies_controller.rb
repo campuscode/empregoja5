1066852a5b126a6c4f73cb5ecc6b8b171f527b03
João Almeida <joaorsalmeida@gmail.com>
2016-07-08 14:08:29 -0300

    User create companies

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,16 @@
+class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.create(params.require(:company).permit(:name, :location,
+                                                             :mail, :phone))
+
+    redirect_to @company
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+end
